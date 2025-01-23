da751c41126acdcd69327afa582c839deb27d4b6
vishnu3021 <vishnurrddy@gmail.com>
2025-01-23 15:08:38 +0530

    main

--- my_project/src/App.js
+++ my_project/src/App.js
@@ -79,7 +79,7 @@ function App() {
         <Route path="/carrents" element={<CarsData />} />
         <Route path="/carrents/:id" element={<Bookcar/>}/>
         <Route path="/carrents/booking/:id" element={<Bookingpage/>}/>
-        <Route path="/carrents/booking/Orderpage/:id" element={<ModelPage />} />
+        {/* <Route path="/carrents/booking/Orderpage/:id" element={<ModelPage />} /> */}
 
         
         <Route path="/rating" element={<Ratingpage />} />
